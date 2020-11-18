d2df519915359cc6ea22b43041b7283e83ff5c92
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-11-18 22:05:56 +1100

    Update tokenDistributor.sol
    [added] freezeFunction modifier to all functions

--- contracts/tokenDistributor.sol
+++ contracts/tokenDistributor.sol
@@ -12,24 +12,23 @@ contract tokenDistributor is xyzToken {
 
     event DispursedTokens(uint indexed totalTokensDistributed, uint256 indexed tokensPerAddress, uint indexed totalParticipationAddresses, address[] addresses);
     
-    function addMyAddress() public {
+    function addMyAddress() public freezeFunction {
         require(addressIsAdded[msg.sender] != true, "address already added");
         addressIsAdded[msg.sender] = true;
         addresses.push(msg.sender);
         totalAddresses++;
     }
 
-    function addAddress(address _address) public onlyOwner {
+    function addAddress(address _address) public onlyOwner freezeFunction {
         require(addressIsAdded[_address] != true, "address already added");
         addressIsAdded[_address] = true;
         addresses.push(_address);
         totalAddresses++;
     }
 
-    function distributeTokens(uint amount) public onlyOwner returns(bool success) {
+    function distributeTokens(uint amount) public onlyOwner freezeFunction returns(bool success) {
         require(amount.div(totalAddresses) != 0, "addresses exceed input amount (minimum one per address)");
         totalSupply = totalSupply.add(amount);
-
         for (uint i = 0; i < addresses.length; i++) {
             address a = addresses[i];
             balanceOf[a] = balanceOf[a].add(amount.div(addresses.length));
